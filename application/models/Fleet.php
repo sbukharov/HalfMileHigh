e8da879c438bb4bee914c7b3a5cadc9e81a7540f
sbukharov <sergey.bukharov@cluberudite.com>
2017-10-05 16:50:27 -0700

    Added models for flights and fleet.

---
+++ application/models/Fleet.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This is a "CMS" model for quotes, but with bogus hard-coded data,
+ * so that we don't have to worry about any database setup.
+ * This would be considered a "mock database" model.
+ *
+ * @author sergey
+ */
+class Fleet extends CI_Model
+{
+
+	// The data comes from http://www.imdb.com/title/tt0094012/
+	// expressed using long-form array notaiton in case students use PHP 5.x
+	var $data = array(
+		'1'	 => array('make'	 => 'Boeing', 'model'	 => '747',
+			'cost'	 => '1400000'),
+		'1'	 => array('make'	 => 'Loghueed Martin', 'model'	 => 'Arrow',
+			'cost'	 => '600000'),
+		'1'	 => array('make'	 => 'Boeing', 'model'	 => 'Model 4',
+			'cost'	 => '1100000'),
+		'1'	 => array('make'	 => 'Boeing', 'model'	 => 'T-148',
+			'cost'	 => '400000'));
+
+	// Constructor
+	public function __construct()
+	{
+		parent::__construct();
+
+		// inject each "record" key into the record itself, for ease of presentation
+		foreach ($this->data as $key => $record)
+		{
+			$record['key'] = $key;
+			$this->data[$key] = $record;
+		}
+	}
+
+	// retrieve a single quote, null if not found
+	public function get($which)
+	{
+		return !isset($this->data[$which]) ? null : $this->data[$which];
+	}
+
+	// retrieve all of the quotes
+	public function all()
+	{
+		return $this->data;
+	}
+
+}
---
+++ application/models/Flights.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This is a "CMS" model for quotes, but with bogus hard-coded data,
+ * so that we don't have to worry about any database setup.
+ * This would be considered a "mock database" model.
+ *
+ * @author sergey
+ */
+class Flights extends CI_Model
+{
+
+	// The data comes from http://www.imdb.com/title/tt0094012/
+	// expressed using long-form array notaiton in case students use PHP 5.x
+	var $data = array(
+		'1'	 => array('from'	 => 'Vancouver', 'to'	 => 'Montreal',
+			'distance'	 => '16700'),
+		'1'	 => array('from'	 => 'Seattle', 'to'	 => 'Montreal',
+			'distance'	 => '18400'),
+		'1'	 => array('from'	 => 'Vancouver', 'to'	 => 'Washington',
+			'distance'	 => '6900'),
+		'1'	 => array('from'	 => 'Washington', 'to'	 => 'Montreal',
+			'distance'	 => '17800'));
+
+	// Constructor
+	public function __construct()
+	{
+		parent::__construct();
+
+		// inject each "record" key into the record itself, for ease of presentation
+		foreach ($this->data as $key => $record)
+		{
+			$record['key'] = $key;
+			$this->data[$key] = $record;
+		}
+	}
+
+	// retrieve a single quote, null if not found
+	public function get($which)
+	{
+		return !isset($this->data[$which]) ? null : $this->data[$which];
+	}
+
+	// retrieve all of the quotes
+	public function all()
+	{
+		return $this->data;
+	}
+
+}
