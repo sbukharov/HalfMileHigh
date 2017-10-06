45238a66afbf0b6569e004757a05848f874654e7
sbukharov <sergey.bukharov@cluberudite.com>
2017-10-05 19:13:59 -0700

    Improved dashboard on homepage to display more information about available flights.

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,14 +1,25 @@
-<table cols="3" class="gallery">
+<h1>Homepage</h1>
+<br /><br />
+
+<h2>Flights available</h2>
+<table>
     <tr>
-        <td class="oneimage">
-            <a href="./data/Katmai_Crater_1980.jpg" data-lightbox="gallery" data-title="Katmai Crater - Mount Katmai, Alaska ... Posted 2014.05.05 by donald, in landscape"><img src="./data/thumb/Katmai_Crater_1980.jpg"/></a>
+        <td>
+            <u>Date</u>
         </td>
-        <td class="oneimage">
-            <a href="./data/Caledonian_orogeny_fold_in_King_Oscar_Fjord.jpg" data-lightbox="gallery" data-title="Caledonian orogeny fold in King Oscar Fjord ... Posted 2014.05.04 by mickey, in landscape"><img src="./data/thumb/Caledonian_orogeny_fold_in_King_Oscar_Fjord.jpg"/></a>
-        </td>
-        <td class="oneimage">
-            <a href="./data/Lands_End_Cape_Cornwall.jpg" data-lightbox="gallery" data-title="Land's End, Cape Cornwall and the Brisons ... Posted 2014.05.04 by mickey, in landscape"><img src="./data/thumb/Lands_End_Cape_Cornwall.jpg"/></a>
+        <td>
+            <u>Number of flights available</u>
         </td>
     </tr>
+    {datearr}
+    <tr>
+        <td>
+            <b>{date}</b>
+        </td>
+        <td>
+            <b>{count}</b>
+        </td>
+    </tr>
+    {/datearr}
 </table>
-<h1>{oct5} flights on oct5</h1>
+
